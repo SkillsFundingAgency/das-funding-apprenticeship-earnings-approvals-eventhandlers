50388773e7170d7940258038a32315334dcdb59f
Michael Young <mick.young1981@googlemail.com>
2022-06-09 09:46:19 +0100

    Added config.

---
+++ src/Functions/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Approvals.EventHandlers.Functions.Configuration;
+
+[assembly: FunctionsStartup(typeof(SFA.DAS.Funding.ApprenticeshipEarnings.Approvals.EventHandlers.Functions.Startup))]
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.Approvals.EventHandlers.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var configuration = serviceProvider.GetService<IConfiguration>();
+
+            var configBuilder = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+
+#if DEBUG
+            configBuilder.AddJsonFile("local.settings.json", optional: true);
+#endif
+
+            if (!configuration["EnvironmentName"].Equals("LOCAL_ACCEPTANCE_TESTS", StringComparison.CurrentCultureIgnoreCase))
+            {
+                configBuilder.AddAzureTableStorage(options =>
+                {
+                    options.ConfigurationKeys = configuration["ConfigNames"].Split(",");
+                    options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
+                    options.EnvironmentName = configuration["EnvironmentName"];
+                    options.PreFixConfigurationKeys = false;
+                });
+            }
+
+            var config = configBuilder.Build();
+            builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), config));
+
+            builder.Services.AddOptions();
+            builder.Services.Configure<ApprenticeshipEarningsApiOptions>(config.GetSection(ApprenticeshipEarningsApiOptions.ApprenticeshipEarningsApi));
+        }
+    }
+}
